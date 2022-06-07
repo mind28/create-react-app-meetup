6674ecbd3a0ca47059d173e07b5578c73b05cdde
mind28 <96925869+mind28@users.noreply.github.com>
2022-06-07 09:09:50 -0400

    excecise 4.3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import CitySearch from './CitySearch';
 import EventList from './EventList';
 import NumberOfEvents from './NumberOfEvents';
+import { extractLocations, getEvents } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <CitySearch />
-      <EventList />
-      <NumberOfEvents/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  }
+
+  updateEvents = (location, numberOfEvents) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      
+      this.setState({
+        events: locationEvents.slice(0, numberOfEvents),
+        currentLocation: location
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
+      </div>
+    );
+  }
 }
 
 export default App;
